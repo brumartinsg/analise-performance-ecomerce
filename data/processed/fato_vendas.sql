eedf10a00fec9f4228e48c8facb67a1096618c5c
brumartinsg <brumartinsg@hotmail.com>
2026-01-08 10:41:04 -0300

    Update fato_vendas.sql

--- data/processed/fato_vendas.sql
+++ data/processed/fato_vendas.sql
@@ -1,20 +1,2 @@
-CREATE OR REPLACE TABLE `analytics.fato_vendas` AS
-SELECT
-    o.order_id,
-    o.order_date,
-    o.user_id,
-
-    oi.product_id,
-    p.product_name,
-    p.category AS product_category,
-
-    oi.quantity,
-    oi.item_price AS unit_price,
-
-    (oi.quantity * oi.item_price) AS revenue
-
-FROM `ecommerce-dataset-483618.raw.orders` o 
-LEFT JOIN `ecommerce-dataset-483618.raw.order_items` oi
-    ON o.order_id = oi.order_id
-LEFT JOIN `ecommerce-dataset-483618.raw.products` p
-    ON oi.product_id = p.product_id
+BigQuery: 
+https://console.cloud.google.com/bigquery?ws=!1m5!1m4!4m3!1secommerce-dataset-483618!2sanalytics!3sfato_vendas
